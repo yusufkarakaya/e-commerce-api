bfc9070d74a8d7f22cf02f312b43ed2796ddc991
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-10-24 11:27:56 -0700

    routing update

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ app.use('/cart', require('./routes/cartRoutes.js'))
 app.use('/auth', require('./routes/authRoutes.js'))
 
 // Serve the index.html file for any other routes (client-side routing support)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
 
 app.all('*', (req, res) => {
   res.status(404)
