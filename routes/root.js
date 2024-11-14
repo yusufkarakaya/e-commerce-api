2c607eeb8a0e1898749352c7289d5371a0a91890
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-11-13 19:01:39 -0800

    path updated

--- routes/root.js
+++ routes/root.js
@@ -3,11 +3,6 @@ const router = express.Router()
 const path = require('path')
 
 router.get('^/$|/index(.html)?', (req, res) => {
-  if (req.path !== '/') {
-    res.redirect('/')
-  } else {
-  }
-
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
 
